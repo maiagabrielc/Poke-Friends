8b5dc3c38263c8873dd1f0580c4e0bfa540eaa7b
Gabriel Carvalho Maia <maiagabrielc@gmail.com>
2022-06-23 21:35:43 -0300

    added card component

---
+++ src/Card.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Card = ({ id, name, email }) => {
+  return (
+    <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+      <img alt='pokemons' src={ `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png` } />
+      <div>
+        <h2>
+          {name}
+        </h2>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
