4cd48673477fddc6d2ab088076e18842ecb8a3e3
Gabriel Carvalho Maia <maiagabrielc@gmail.com>
2022-06-23 22:54:11 -0300

    added errorBoudry component

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
+import ErrorBoundry from '../components/ErrorBoundry';
 
 
 class App extends Component {
@@ -34,7 +35,9 @@ class App extends Component {
         <h1>PokeFriends</h1>
         <SearchBox searchChange={ this.onSearchChange }/>
         <Scroll>
-          <CardList pokemons={filteredPokemons} />
+          <ErrorBoundry>
+            <CardList pokemons={filteredPokemons} />
+          </ErrorBoundry>
         </Scroll>
       </div>
     );
